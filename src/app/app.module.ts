9d35a191331bac0cb59488767fdc4bd09e16bb41
Vương Nguyễn <vuongnqv@gmail.com>
2022-04-09 18:09:44 +0700

    add template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHomeComponent } from './component/user-home/user-home.component';
+import { AdminHomeComponent } from './component/admin-home/admin-home.component';
+import { WebsiteHomeComponent } from './component/website-home/website-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHomeComponent,
+    AdminHomeComponent,
+    WebsiteHomeComponent
   ],
   imports: [
     BrowserModule,
