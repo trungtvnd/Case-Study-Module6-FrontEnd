d3050ccbe0fbeec66348927739b1bb6b94cd04ae
Vương Nguyễn <vuongnqv@gmail.com>
2022-04-11 09:36:20 +0700

    update template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserHomeComponent } from './component/user-home/user-home.component';
-import { AdminHomeComponent } from './component/admin-home/admin-home.component';
-import { WebsiteHomeComponent } from './component/website-home/website-home.component';
+import { UserHomeComponent } from './dashboard/user-home/user-home.component';
+import { AdminHomeComponent } from './dashboard/admin-home/admin-home.component';
+import { WebsiteHomeComponent } from './dashboard/website-home/website-home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserHomeComponent,
     AdminHomeComponent,
-    WebsiteHomeComponent
+    WebsiteHomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
